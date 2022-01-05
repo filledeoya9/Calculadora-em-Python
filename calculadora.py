eb0cf8e520248f8c94dadb4e3eea42a024557ba2
Juliana Rodrigues <87615162+filledeoya9@users.noreply.github.com>
2022-01-05 15:37:58 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,48 @@
+print ("\n***** Calculadora em Python *****")
+
+def add (x,y):
+    return x+y 
+
+def subtract (x,y):
+    return x-y
+
+def multiply (x,y):
+    return x*y
+
+def divide (x,y):
+    return x/y
+
+print ("\n Selecione o número da opção desejada\n")
+
+print ("1- Soma")
+print ("2- Subtração")
+print ("3- Multiplicação")
+print  ("4- Divisão")
+
+escolha = input ("\n Digite sua opção  (1/2/3/4): ")
+
+num1= int (input("\n Digite o primeiro número: "))
+num2 =int (int (input("\n Digite o segundo número: ")))mkadir
+
+if escolha == '1':
+    print("\n")
+    print ( num1,"+", num2, "=", add (num1,num2))
+    print ("\n")
+
+elif escolha == '2':
+    print("\n")
+    print ( num1,"-", num2, "=", subtract (num1,num2))
+    print ("\n")
+
+elif escolha == '3':
+    print("\n")
+    print ( num1,"*", num2, "=", multiply (num1,num2))
+    print ("\n")
+
+elif escolha == '4':
+    print("\n")
+    print ( num1,"/", num2, "=", divide (num1,num2))
+    print ("\n")
+
+else:
+    print("\n Opção inválida!")
